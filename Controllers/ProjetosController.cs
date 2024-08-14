552152675068871a952f324005d5fd688919cefb
joseluizs <joseluiz.smarques@gmail.com>
2024-08-14 20:09:56 -0300

    Subir projeto para validação no github

--- Controllers/ProjetosController.cs
+++ Controllers/ProjetosController.cs
@@ -23,5 +23,67 @@ namespace Exo.WebApi.Controllers
         {
             return Ok(_projetoRepository.Listar());
         }
+
+        [HttpGet("{id}")]
+        public IActionResult BuscarPorId(int id)
+        {
+            var projeto = _projetoRepository.BuscarPorId(id);
+            if (projeto == null)
+            {
+                return NotFound("Projeto não encontrado!");
+            }
+            return Ok(projeto);
+        }
+
+        public IActionResult Cadastrar(Projeto projeto)
+        {
+            try
+            {
+                _projetoRepository.Cadastrar(projeto);
+                return StatusCode(201, "Projeto cadastrado com sucesso!");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Atualizar(int id, Projeto projeto)
+        {
+            try
+            {
+                var projetoBuscado = _projetoRepository.BuscarPorId(id);
+                if (projetoBuscado == null)
+                {
+                    return NotFound("Projeto não encontrado!");
+                }
+                _projetoRepository.Atualizar(id, projeto);
+                return StatusCode(201, "Projeto atualizado com sucesso!");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Deletar(int id)
+        {
+            try
+            {
+                var projetoBuscado = _projetoRepository.BuscarPorId(id);
+                if (projetoBuscado == null)
+                {
+                    return NotFound("Projeto não encontrado!");
+                }
+                _projetoRepository.Deletar(id);
+                return StatusCode(201, "Projeto deletado com sucesso!");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
     }
 }
