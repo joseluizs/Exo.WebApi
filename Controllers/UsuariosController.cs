dd537b3c6ab86f8c51933de60928131ec57aa3f3
joseluizs <joseluiz.smarques@gmail.com>
2024-08-15 10:40:54 -0300

    Criar métodos de login e Cors

--- Controllers/UsuariosController.cs
+++ Controllers/UsuariosController.cs
@@ -3,6 +3,9 @@ using Exo.WebApi.Repositories;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 
 namespace Exo.WebApi.Controllers
 {
@@ -24,11 +27,42 @@ namespace Exo.WebApi.Controllers
             return Ok(_usuarioRepository.Listar());
         }
 
+        [Authorize]
         [HttpPost]
         public IActionResult Cadastrar(Usuario usuario)
         {
             _usuarioRepository.Cadastrar(usuario);
             return StatusCode(201, "Usuario Cadastrado com sucesso!");
+        }
+
+        public IActionResult Post(Usuario usuario)
+        {
+            Usuario usuarioBuscado = _usuarioRepository.Login(usuario.Email, usuario.Senha);
+            if (usuarioBuscado == null)
+            {
+                return NotFound("E-mail ou senha inválidos!");
+            }
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Email, usuarioBuscado.Email),
+                new Claim(JwtRegisteredClaimNames.Jti, usuarioBuscado.Id.ToString()),
+            };
+
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("exoapi-chave-autenticação"));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            //gerar token
+            var token = new JwtSecurityToken(
+                issuer: "exoapi.webapi", //emissor do token
+                audience: "exoapi.webapi", //destinatario do token
+                claims: claims, //dados definidos acima
+                expires: DateTime.Now.AddMinutes(30), //tempo de expiração
+                signingCredentials: creds //credenciais do token
+            );
+            return Ok(
+                new { token = new JwtSecurityTokenHandler().WriteToken(token) }
+            );
+
         }
 
         [HttpGet("{id}")]
@@ -42,6 +76,7 @@ namespace Exo.WebApi.Controllers
             return Ok(usuario);
         }
 
+        [Authorize]
         [HttpPut("{id}")]
         public IActionResult Atualizar(int id, Usuario usuario)
         {
@@ -49,6 +84,7 @@ namespace Exo.WebApi.Controllers
             return StatusCode(204, "Usuario Atualizado com sucesso!");
         }
 
+        [Authorize]
         [HttpDelete("{id}")]
         public IActionResult Deletar(int id)
         {
--- Program.cs
+++ Program.cs
@@ -1,16 +1,54 @@
 using Exo.WebApi.Contexts;
 using Exo.WebApi.Repositories;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddScoped<ExoContext, ExoContext>();
 builder.Services.AddControllers();
+
+//Forma de autenticação
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = "JwtBearer";
+    options.DefaultChallengeScheme = "JwtBearer";
+})
+//Parametros de validação do token
+.AddJwtBearer("JwtBearer", options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        //valida quen esta solicintando
+        ValidateIssuerSigningKey = true,
+        //validaque quem esta recebendo
+        ValidateAudience = true,
+        //define o tempo de expiração
+        ValidateLifetime = true,
+        //criptografia a validação
+        IssuerSigningKey = new
+            SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("exoapi-chave-autenticação")),
+        //valida o tempo de expiração
+        ClockSkew = TimeSpan.FromMinutes(30),
+        //nome do issuer
+        ValidIssuer = "exoapi.webapi",
+        //nome do audience p/destino
+        ValidAudience = "exoapi.webapi"
+    };
+
+});
+
+
 builder.Services.AddTransient<ProjetoRepository, ProjetoRepository>();
 builder.Services.AddTransient<UsuarioRepository, UsuarioRepository>();
 
 var app = builder.Build();
 
 app.UseRouting();
+
+//habilita a autenticação
+app.UseAuthentication();
+//habilita a autorização
+app.UseAuthorization();
 
 app.UseEndpoints(endpoints =>
 {
